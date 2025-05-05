f160cc8a66fd60c902f043f1794e76f5bd63a92f
bianliao <138651187+bianliao@users.noreply.github.com>
2025-05-05 21:56:11 +0800

    ui/auth page style

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,18 +1,24 @@
 import AuthForm from '../components/AuthForm';
 
 const Page: React.FC = () => {
+    let banner = "w-full px-4 py-6  bg-indigo-200"
+    let authForm = "max-w-sm"
+    let styleH1 = "text-2xl text-indigo-800 font-black pb-1 m-auto text-center";
     let styleH2 = "text-lg text-indigo-800 font-black pb-1";
 
     return (
         <main>
-            <div>
+            <section className={banner}>
+                <h1 className={styleH1}>React 練習用專案</h1>
+            </section>
+            <section className={authForm}>
                 <h2 className={styleH2}>註冊帳號</h2>
                 <AuthForm type="signup" />
-            </div>
-            <div>
+            </section>
+            <section className={authForm}>
                 <h2 className={styleH2}>登入系統</h2>
                 <AuthForm type="signin" />
-            </div>
+            </section>
         </main>
     );
 };
